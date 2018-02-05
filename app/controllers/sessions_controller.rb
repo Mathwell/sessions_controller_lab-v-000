c71ea315c9af49db7536b0598e4b0e269ee4decb
Olena Ageyeva <eageeva@hotmail.com>
2018-02-05 02:46:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,12 @@ class SessionsController <ApplicationController
   end
 
   def create
+    if params[:name] && !params[:name].empty?
+      session[:name]=params[:name]
+      redirect_to controller: 'application', action: 'hello'
+    else
+      redirect_to controller: 'sessions', action: 'new'
+    end
   end
 
   def destroy
